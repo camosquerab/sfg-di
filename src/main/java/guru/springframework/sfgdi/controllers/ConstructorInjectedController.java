3478a76dbd438e4f77305c8ba37083846925cb73
Camilo Alfonso Mosquera Benavides <adrianricardo.rojas.contractor.cidentity@bbva.com>
2023-10-16 17:31:40 -0500

    Dependency injection using Spring Framework

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
@@ -9,6 +11,7 @@ public class ConstructorInjectedController {
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
+
 
     public String getGreeting() {
         return greetingService.sayGreeting();
--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -1,16 +1,17 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
-
-    public void setGreetingService(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
 
     private GreetingService greetingService;
 
-    public SetterInjectedController() {
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
 
     public String getGreeting(){
